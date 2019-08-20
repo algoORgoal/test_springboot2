65b343f8a7a4ad8c309da017426e51a883d4fa00
algoORgoal <qufstar46@gmail.com>
2019-08-20 11:33:52 +0900

    commit

--- src/main/java/com/example/test_springboot2/controller/MemberController.java
+++ src/main/java/com/example/test_springboot2/controller/MemberController.java
@@ -6,7 +6,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MemberController {
     @RequestMapping(value = "/member/list")
-    public void memberAction() {
+    public String list() {
+        return "list";
+    }
 
+    @RequestMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/logout")
+    public String logout() {
+        return "logout";
+    }
+
+    @RequestMapping(value = "/register")
+    public String register() {
+        return "register";
+    }
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
     }
 }
