572dd5feb296ba67d99061fab982bc78380c3c47
algoORgoal <qufstar46@gmail.com>
2019-08-20 14:09:07 +0900

    create controllers

---
+++ src/main/java/com/example/test_springboot2/controller/CommonCodeController.java
@@ -0,0 +1,14 @@
+/*
+ * package com.example.test_springboot2.controller;
+ * 
+ * import org.springframework.stereotype.Controller; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RequestMethod;
+ * 
+ * @Controller
+ * 
+ * @RequestMapping(value = "/commonCode") public class CommonCodeController {
+ * 
+ * @RequestMapping(value = "/edit", method = { RequestMethod.GET,
+ * RequestMethod.POST }) public void edit() { } }
+ */
---
+++ src/main/java/com/example/test_springboot2/controller/MemberController2.java
@@ -0,0 +1,13 @@
+package com.example.test_springboot2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/commonCode")
+public class MemberController2 {
+    @RequestMapping(value = { "/edit", "/read", "/list" }, method = { RequestMethod.GET, RequestMethod.POST })
+    public void actionMethod() {
+    }
+}
